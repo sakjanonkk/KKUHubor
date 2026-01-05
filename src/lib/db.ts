028f51d8cb5f3141f2b509a72cb1b0cd0cd6478f
sakjanonkk <sakjanon.k@kkumail.com>
2026-01-06 04:04:20 +0700

    fix db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,18 +1,12 @@
-import { Pool } from 'pg';
+import { Pool } from "pg";
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '5432'),
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
 });
 
-// Use a global variable to store the pool instance in development
-// to avoid creating multiple connections during hot-reloading.
 let db: Pool;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   db = pool;
 } else {
   if (!(global as any).postgresPool) {
